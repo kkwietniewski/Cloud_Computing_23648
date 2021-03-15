1cabea94b3a14d8bb37ce9edac954363abea20ee
Kacper Kwietniewski <48916089+kkwietniewski@users.noreply.github.com>
2021-03-15 19:44:01 +0100

    Azure: added sql server, database, App: added connectionString, Person class, dbcontext, options.UseSqlServer, Initial Migration, app published, PeopleController: get, get-id, create, update, delete

---
+++ lab1/FirstTimeAzure/PeopleWebApp/Context/AzureDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeopleWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleWebApp.Context
+{
+    public class AzureDbContext : DbContext
+    {
+        public AzureDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected AzureDbContext()
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
---
+++ lab1/FirstTimeAzure/PeopleWebApp/Controllers/PeopleController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PeopleWebApp.Context;
+using PeopleWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly AzureDbContext db;
+
+        public PeopleController(AzureDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var people = db.People.ToList();
+            return Ok(people);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var findedPeople = db.People.FirstOrDefault(people => people.PersonId == id);
+            if (findedPeople == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(findedPeople);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Person person)
+        {
+            if(person == null)
+            {
+                return BadRequest();
+            }
+
+            db.People.Add(person);
+            db.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = person.PersonId}, person);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Modify(int id, Person person)
+        {
+            var findedPeople = db.People.FirstOrDefault(people => people.PersonId == id);
+            if (findedPeople == null)
+            {
+                return BadRequest();
+            }
+            else 
+            {
+                findedPeople.FirstName = person.FirstName;
+                findedPeople.LastName = person.LastName;
+                db.SaveChanges();
+            }
+
+            return Ok(findedPeople);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var findedPeople = db.People.FirstOrDefault(people => people.PersonId == id);
+            if (findedPeople == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                db.People.Remove(findedPeople);
+                db.SaveChanges();
+            }
+
+            return Ok(findedPeople);
+        }
+    }
+}
